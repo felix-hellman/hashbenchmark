45b70de4b1e7413d5948211f9a374bc59a7a2630
felix-hellman <f0722358227@gmail.com>
2016-04-30 12:54:16 +0200

    Fixed auto + removed some duplicate code

--- func.cpp
+++ func.cpp
@@ -1,10 +1,5 @@
 #include "func.h"
-void swap(int &a, int &b)
-{
-	int tmp = a;
-	a = b;
-	b = tmp;
-}
+
 void printHelp()
 {
 	cout << "To use this program edit the hashFunction.cpp file with the function to test" << endl;
@@ -13,61 +8,6 @@ void printHelp()
 	cout << "To run the hash in single table size mode use -s <tablesize> " << endl;
 	cout << "To run the hash in intervall table size mode use -i <minTableSize> <maxTableSize>" << endl;
 	cout << "If no file is specified then the program will try to open the file \"input\" as default" << endl;  
-}
-bool isNumber(const string &toCheck)
-{
-	for(unsigned int i = 0; i < toCheck.length(); i++)
-		if(toCheck[i] < 0x30 || toCheck[i] > 0x39)
-			return false; 
-	return true;
-}
-inline bool exists(const string &name)
-{
-	ifstream f(name.c_str());
-	return f.good();
-}
-bool contains(const string &word, char ** arr,int size)
-{
-	for(int i = 0; i < size; i++)
-		if(string(arr[i]) == word)
-			return true;	
-	return false;
-}
-bool argumentHandler(int argc, char ** argv, string &filepath, int &minTableSize, int &maxTableSize)
-{
-	if(argc < 2)
-		return true;
-	else
-	{
-		for(int i = 1; i < argc; i++)
-		{
-			if(string(argv[i]) == string("-f"))
-			{
-				if(i >= argc-1)
-					return true;
-				filepath = argv[i+1];
-				if(!exists(filepath))
-					return true;	
-			}
-			else if(string(argv[i]) == string("-i"))
-			{
-				if(!isNumber((string(argv[i+1]))) || !isNumber((string(argv[i+2]))) || contains("-s",argv,argc))
-					return true;
-				minTableSize = stoi(string(argv[i+1]));
-				maxTableSize = stoi(string(argv[i+2]));
-				if(minTableSize > maxTableSize)
-					swap(minTableSize,maxTableSize);
-			}
-			else if(string(argv[i]) == "-s")
-			{
-				if(!isNumber(string(argv[i+1])) || contains("-i",argv,argc))
-					return true;
-				minTableSize = stoi(string(argv[i+1]));
-				maxTableSize = minTableSize;
-			}		
-		}
-	}
-	return false;
 }
 void inputFromFile(vector<string> &stringVec, const string &filepath)
 {
@@ -88,29 +28,26 @@ void hashVectorFill(const vector<string> &stringVec, vector<unsigned int> &hashV
 	
 	while(!hashVector.empty())
 		hashVector.pop_back();
-	for(unsigned int i = 0; i < stringVec.size(); i++)
+	for(auto i = 0; i < stringVec.size(); i++)
 		hashVector.push_back(hashFunction(stringVec[i])%tablesize);
 }
 int hashVectorCollisions(const vector<unsigned int> &hashVector,vector<string> &reportVector)
 {
-	int count;
 	while(!reportVector.empty())
 		reportVector.pop_back();
-	int finalCount = 0;
-	bool skip = false;
-	int maxCount = 0;
-	unsigned int min = 0xFFFFFFFF;
-	unsigned int max = 0;
-	for(unsigned int i = 0; i < hashVector.size(); i++)
+	auto skip = false;
+	auto  max = 0, finalCount = 0, count = 0, maxCount = 0;
+	auto min = 0xFFFFFFFF;
+	for(auto i = 0; i < hashVector.size(); i++)
 	{
 		skip = false;
-		for(unsigned int x = 0; x < i; x++)
+		for(auto x = 0; x < i; x++)
 			if(hashVector[i] == hashVector[x])
 				skip = true;
 		count = 0;
 		if(!skip)
 		{
-			for(unsigned int x = 0; x < hashVector.size(); x++)
+			for(auto x = 0; x < hashVector.size(); x++)
 			{
 				if(hashVector[i] == hashVector[x])
 				{
@@ -118,12 +55,7 @@ int hashVectorCollisions(const vector<unsigned int> &hashVector,vector<string> &
 				}
 			}
 			if(count > 1)
-			{
-			//	stringstream ss;
-			//	ss << "The hash : \t" << hashVector[i] << "\t :: \t " << count << endl;
-			//	reportVector.push_back(ss.str());
-				finalCount += count;
-			}
+				finalCount += count;	
 		}
 		if(count > maxCount)
 			maxCount = count;
@@ -141,8 +73,7 @@ int hashVectorCollisions(const vector<unsigned int> &hashVector,vector<string> &
 }
 void checkClusters(const vector<unsigned int> &hashVector, string &report)
 {
-	auto max = 0, x = 0;
-	int maxCluster = 1, currentCluster = 1;
+	auto max = 0, x = 0, maxCluster = 1, currentCluster = 1;
 	for(auto i = 0; i < hashVector.size(); i++)
 		if(hashVector[i] > max)
 			max = hashVector[i];
@@ -156,7 +87,7 @@ void checkClusters(const vector<unsigned int> &hashVector, string &report)
 			x = (x+1)%(max+1);
 		arr[x] = true;
 	}
-	for(int i = 0; i < max; i++)
+	for(auto i = 0; i < max; i++)
 	{	
 		if(arr[i] && arr[i+1])
 		{
